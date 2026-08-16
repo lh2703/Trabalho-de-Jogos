25e410f9c604da4054acfef935329574fa6a5f9e
Luís Henrique <carvalholuis839@gmail.com>
2026-08-16 17:21:37 -0300

    att

--- Trab1/Intro.py
+++ Trab1/Intro.py
@@ -1,31 +1,35 @@
 # Inicialização
-import pygame 
+import pygame
 import random
 pygame.init()
 pygame.font.init()
 
 
 
-font = font = pygame.font.Font(None, 50)
-Nome = "Seu Nome"
-rect =  (260, 100, 175, 35)
+font = font = pygame.font.Font(None, 40)
+Nome = "Luís Henrique"
 
 random.seed(Nome)
-x, y =  random.randint(0, 500), random.randint(0, 400)
+x, y =  random.randint(265, 280), random.randint(105, 110)
 
 print(y)
 
 # Cria a janela
 WIDTH   =  800; HEIGHT =  600
-screen = pygame.display.set_mode((WIDTH, HEIGHT))  
+screen = pygame.display.set_mode((WIDTH , HEIGHT))
+
+texto = font.render(Nome, True, (0,0,0))
+rect = texto.get_rect(center=(WIDTH/2, HEIGHT/2))
+
 
 #loop
-while True: 
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            pygame.quit()
             exit()
         # Desenha
-        screen.fill((30, 30, 30))
+        screen.fill((50, 50, 50))
         pygame.draw.rect(screen, (255,255,255), rect)
-        screen.blit(font.render(Nome, True, (0,0,0)), (x, y))
+        screen.blit(texto, rect)
         pygame.display.flip()
