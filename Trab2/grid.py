92876a4c0d73e4978781ab3fbbd0ecb1d93231da
Luís Henrique <carvalholuis839@gmail.com>
2026-08-28 14:03:59 -0300

    adiciona trabalho 2

--- Trab2/grid.py
+++ Trab2/grid.py
@@ -1,58 +1,220 @@
-# Classe base abstrata (Abstract Base Class)
+import pygame
 from abc import ABC, abstractmethod
 
+CELL_SIZE = 50
 
-# objeto herda de classe abstrata
-## nunca pode ser criada, só as filhas
-class obj (ABC):
 
-    def __init__(self, x, y, sprites):
+class obj(ABC):
+    def __init__(self, x, y, sprites=None):
         self.x = x
         self.y = y
-        #lista
-        self.sprites = sprites
-
+        self.sprites = sprites if sprites is not None else []
 
     def draw(self, screen):
-        for s in self.sprites:
-            screen.blit(self.sprites, (self.x, self.y))
+        if self.sprites:
+            screen.blit(self.sprites[0], (self.x, self.y))
 
-    # avisa que o método é abstato e precisa ser feito pelos filhos
     @abstractmethod
     def update(self, dt):
         pass
 
-class Grid (obj):
 
-    # só avisa a construtora da mãe o que fazer
-    # pode, e deve ser extendido para outras caracteristicas nescessárias
-    def __init__(self, x, y, sprites, grid_size):
-        # bom lugar para criar uma matriz de celulas
-        super().__init__(x, y, sprites)
-
-    def draw(self, screen):
-
-        # chama o desenha já pronto de obj, mas pode ser extendido
-        return super().draw(screen)
-
-    def update(self, dt):
-        # não chama o de super, porque ele não implementa, crie seu próprio
-        return 
-
-
-class Cell (obj):
-
-    # só avisa a construtora da mãe o que fazer
-    # pode, e deve ser extendido para outras caracteristicas nescessárias
-    def __init__(self, x, y, sprites, grid_size):
-        # bom lugar para definir coisas como o fundo da céula
-        super().__init__(x, y, sprites)
+class Cell(obj):
+    def __init__(self, x, y, cell_type=' '):
+        super().__init__(x, y, [])
+        self.cell_type = cell_type
+        self.rect = pygame.Rect(self.x, self.y, CELL_SIZE, CELL_SIZE)
 
     def draw(self, screen):
+        pygame.draw.rect(screen, (45, 45, 55), self.rect)
+        pygame.draw.rect(screen, (35, 35, 45), self.rect, 1)
 
-        # chama o desenha já pronto de obj, mas pode ser extendido
-        return super().draw(screen)
+        if self.cell_type == '#':
+            pygame.draw.rect(screen, (65, 75, 90), self.rect)
+            pygame.draw.rect(screen, (90, 100, 120), self.rect, 2)
+
+        elif self.cell_type == '.':
+            pygame.draw.circle(screen, (70, 190, 110), self.rect.center, 8)
+
+        elif self.cell_type in ['$', '*']:
+            m = 6
+            box_rect = pygame.Rect(self.x + m, self.y + m, CELL_SIZE - 2 * m, CELL_SIZE - 2 * m)
+            cor = (220, 170, 40) if self.cell_type == '*' else (150, 90, 40)
+            borda = (240, 220, 100) if self.cell_type == '*' else (90, 50, 20)
+
+            pygame.draw.rect(screen, cor, box_rect, border_radius=4)
+            pygame.draw.rect(screen, borda, box_rect, 2, border_radius=4)
+            pygame.draw.line(screen, borda, box_rect.topleft, box_rect.bottomright, 2)
+            pygame.draw.line(screen, borda, box_rect.bottomleft, box_rect.topright, 2)
 
     def update(self, dt):
-        # não chama o de super, porque ele não implementa, crie seu próprio
-        return 
+        pass
+
+
+class Player(obj):
+    def __init__(self, grid_col, grid_row, sprites_dict, offset_x, offset_y):
+        self.col = grid_col
+        self.row = grid_row
+        self.offset_x = offset_x
+        self.offset_y = offset_y
+        self.sprites_dict = sprites_dict
+
+        super().__init__(
+            offset_x + grid_col * CELL_SIZE,
+            offset_y + grid_row * CELL_SIZE,
+            list(sprites_dict.values())
+        )
+
+        self.virado_esquerda = False
+        self.tempo_anim = 0.0
+        self.frame_atual = 'base'
+        self.tocando_som = False
+        self.tempo_som = 0.0
+
+    def mover(self, col, row, dx):
+        self.col = col
+        self.row = row
+        self.x = self.offset_x + col * CELL_SIZE
+        self.y = self.offset_y + row * CELL_SIZE
+        if dx < 0:
+            self.virado_esquerda = True
+        elif dx > 0:
+            self.virado_esquerda = False
+
+    def update(self, dt):
+        self.tempo_anim += dt
+
+        if self.tocando_som:
+            self.frame_atual = 'quack'
+            self.tempo_som -= dt
+            if self.tempo_som <= 0:
+                self.tocando_som = False
+        else:
+            ciclo = int(self.tempo_anim * 4) % 10
+            if ciclo in [0, 1]:
+                self.frame_atual = 'step'
+            elif ciclo == 5:
+                self.frame_atual = 'blink'
+            elif ciclo == 8:
+                self.frame_atual = 'wing'
+            else:
+                self.frame_atual = 'base'
+
+    def draw(self, screen):
+        sprite = self.sprites_dict.get(self.frame_atual, self.sprites_dict['base'])
+        if self.virado_esquerda:
+            sprite = pygame.transform.flip(sprite, True, False)
+
+        ox = (CELL_SIZE - sprite.get_width()) // 2
+        oy = (CELL_SIZE - sprite.get_height()) // 2
+        screen.blit(sprite, (self.x + ox, self.y + oy))
+
+
+class Grid(obj):
+    def __init__(self, x, y, layout, sprites_dict):
+        super().__init__(x, y, [])
+        self.linhas = len(layout)
+        self.colunas = len(layout[0])
+        self.matriz = []
+        self.sprites_dict = sprites_dict
+        self.movimentos = 0
+        self.venceu = False
+
+        pos_pato = (0, 0)
+
+        for r in range(self.linhas):
+            linha = []
+            for c in range(self.colunas):
+                char = layout[r][c]
+                cx = x + c * CELL_SIZE
+                cy = y + r * CELL_SIZE
+
+                if char == '@':
+                    tipo = ' '
+                    pos_pato = (c, r)
+                elif char == '+':
+                    tipo = '.'
+                    pos_pato = (c, r)
+                else:
+                    tipo = char
+
+                linha.append(Cell(cx, cy, tipo))
+            self.matriz.append(linha)
+
+        self.player = Player(pos_pato[0], pos_pato[1], sprites_dict, x, y)
+
+    def pos_valida(self, c, r):
+        return 0 <= r < self.linhas and 0 <= c < self.colunas
+
+    def tentar_mover(self, dx, dy):
+        if self.venceu:
+            return False
+
+        nc = self.player.col + dx
+        nr = self.player.row + dy
+
+        if not self.pos_valida(nc, nr):
+            return False
+
+        celula_alvo = self.matriz[nr][nc]
+
+        if celula_alvo.cell_type == '#':
+            return False
+
+        if celula_alvo.cell_type in ['$', '*']:
+            proxc = nc + dx
+            proxr = nr + dy
+
+            if not self.pos_valida(proxc, proxr):
+                return False
+
+            destino_caixa = self.matriz[proxr][proxc]
+            if destino_caixa.cell_type in [' ', '.']:
+                destino_caixa.cell_type = '*' if destino_caixa.cell_type == '.' else '$'
+                celula_alvo.cell_type = '.' if celula_alvo.cell_type == '*' else ' '
+
+                self.player.mover(nc, nr, dx)
+                self.player.tocando_som = True
+                self.player.tempo_som = 0.2
+                self.movimentos += 1
+                self.checar_vitoria()
+                return True
+            return False
+
+        if celula_alvo.cell_type in [' ', '.']:
+            self.player.mover(nc, nr, dx)
+            self.movimentos += 1
+            return True
+
+        return False
+
+    def clique_mouse(self, pos):
+        mx, my = pos
+        c = (mx - self.x) // CELL_SIZE
+        r = (my - self.y) // CELL_SIZE
+
+        if self.pos_valida(c, r):
+            dx = c - self.player.col
+            dy = r - self.player.row
+            if abs(dx) + abs(dy) == 1:
+                self.tentar_mover(dx, dy)
+
+    def checar_vitoria(self):
+        for r in range(self.linhas):
+            for c in range(self.colunas):
+                if self.matriz[r][c].cell_type == '$':
+                    return False
+        self.venceu = True
+        return True
+
+    def update(self, dt):
+        for linha in self.matriz:
+            for c in linha:
+                c.update(dt)
+        self.player.update(dt)
+
+    def draw(self, screen):
+        for linha in self.matriz:
+            for c in linha:
+                c.draw(screen)
+        self.player.draw(screen)
--- Trab2/main.py
+++ Trab2/main.py
@@ -1,66 +1,196 @@
 import pygame
-from grid import Grid, Cell
-
+import sys
+import os
+from grid import Grid
 
 pygame.init()
 pygame.font.init()
 
-WIDTH   =  800; HEIGHT =  600
-screen = pygame.display.set_mode((WIDTH, HEIGHT))  
+LARGURA = 800
+ALTURA = 600
+screen = pygame.display.set_mode((LARGURA, ALTURA))
+pygame.display.set_caption("Trabalho 2 - Sokoban")
+clock = pygame.time.Clock()
 
-# caso precise de usar fontes na main, descomente
+fonte = pygame.font.SysFont("Arial", 20, bold=True)
+fonte_grande = pygame.font.SysFont("Arial", 32, bold=True)
+fonte_titulo = pygame.font.SysFont("Arial", 42, bold=True)
 
-#font_size
-#font = pygame.font.Font(None, font_size)
-
-# caso precise carregar imagens na main, descomente
-
-#idle = pygame.image.load("images/duck/duck.png").convert_alpha()
-#step = pygame.image.load("images/duck/step.png").convert_alpha()
-#etc
+PASTA_PATO = os.path.join("images", "duck")
 
 
-#numero de celulas
-grid_size = (5, 10)
+def carregar_img(nome, tamanho=(40, 40)):
+    caminho = os.path.join(PASTA_PATO, nome)
+    try:
+        img = pygame.image.load(caminho).convert_alpha()
+        return pygame.transform.scale(img, tamanho)
+    except Exception:
+        s = pygame.Surface(tamanho, pygame.SRCALPHA)
+        pygame.draw.circle(s, (240, 200, 0), (tamanho[0] // 2, tamanho[1] // 2), 15)
+        return s
 
 
-# Cria a janela
-WIDTH   =  800; HEIGHT =  600
-screen = pygame.display.set_mode((WIDTH, HEIGHT))  
+sprites = {
+    'base': carregar_img("base.png"),
+    'step': carregar_img("step.png"),
+    'crouch': carregar_img("crouch.png"),
+    'blink': carregar_img("blink.png"),
+    'quack': carregar_img("quack.png"),
+    'wing': carregar_img("wing.png"),
+}
 
-#criar objetos, adicione eles a lista
-objects = []
+MAPAS = [
+    [
+        "#######",
+        "#     #",
+        "# @   #",
+        "# $ . #",
+        "# $ . #",
+        "#     #",
+        "#######"
+    ],
+    [
+        "#######",
+        "#     #",
+        "#  .  #",
+        "# $ $ #",
+        "#  @  #",
+        "#  .  #",
+        "#######"
+    ],
+    [
+        "########",
+        "#      #",
+        "#  . . #",
+        "# $ $  #",
+        "#  @ $ #",
+        "#   .  #",
+        "#      #",
+        "########"
+    ]
+]
 
-while True: 
+nivel_atual = 0
+jogo_finalizado = False
+
+
+def criar_fase(idx):
+    layout = MAPAS[idx]
+    w = len(layout[0]) * 50
+    h = len(layout) * 50
+    start_x = (LARGURA - w) // 2
+    start_y = (ALTURA - h) // 2 + 20
+    return Grid(start_x, start_y, layout, sprites)
+
+
+grid = criar_fase(nivel_atual)
+
+btn_reset = pygame.Rect(LARGURA - 160, 20, 130, 35)
+btn_prox = pygame.Rect(LARGURA - 160, 65, 130, 35)
+btn_recomecar = pygame.Rect(LARGURA // 2 - 100, ALTURA // 2 + 50, 200, 45)
+
+while True:
+    dt = clock.tick(60) / 1000.0
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            exit()
+            pygame.quit()
+            sys.exit()
 
-        # uso do mouse é obrigatório
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            if pygame.mouse.get_pressed()[0]: # 0 botão esquedo 2, direito
-                pass # faça algo
+            if event.button == 1:
+                m_pos = pygame.mouse.get_pos()
 
-        #caso queira usar levantar o mouse, descomente
-        #elif event.type == pygame.MOUSEBUTTONUP:
-        #                    exit()
+                if jogo_finalizado:
+                    if btn_recomecar.collidepoint(m_pos):
+                        nivel_atual = 0
+                        jogo_finalizado = False
+                        grid = criar_fase(nivel_atual)
+                else:
+                    if btn_reset.collidepoint(m_pos):
+                        grid = criar_fase(nivel_atual)
+                    elif btn_prox.collidepoint(m_pos) and grid.venceu:
+                        if nivel_atual + 1 < len(MAPAS):
+                            nivel_atual += 1
+                            grid = criar_fase(nivel_atual)
+                        else:
+                            jogo_finalizado = True
+                    else:
+                        grid.clique_mouse(m_pos)
 
-
-        # uso do teclado para controle é obrigatório
         elif event.type == pygame.KEYDOWN:
-            #inclua outras funcionalidades para outras téclas
             if event.key == pygame.K_ESCAPE:
-                exit()
+                pygame.quit()
+                sys.exit()
 
-        #atualiza
-        for obj in objects:
-            obj.update(1)
+            if jogo_finalizado:
+                if event.key in [pygame.K_SPACE, pygame.K_RETURN, pygame.K_r]:
+                    nivel_atual = 0
+                    jogo_finalizado = False
+                    grid = criar_fase(nivel_atual)
+            else:
+                if event.key in [pygame.K_LEFT, pygame.K_a]:
+                    grid.tentar_mover(-1, 0)
+                elif event.key in [pygame.K_RIGHT, pygame.K_d]:
+                    grid.tentar_mover(1, 0)
+                elif event.key in [pygame.K_UP, pygame.K_w]:
+                    grid.tentar_mover(0, -1)
+                elif event.key in [pygame.K_DOWN, pygame.K_s]:
+                    grid.tentar_mover(0, 1)
+                elif event.key == pygame.K_r:
+                    grid = criar_fase(nivel_atual)
+                elif event.key in [pygame.K_SPACE, pygame.K_RETURN] and grid.venceu:
+                    if nivel_atual + 1 < len(MAPAS):
+                        nivel_atual += 1
+                        grid = criar_fase(nivel_atual)
+                    else:
+                        jogo_finalizado = True
 
-        # Desenha
-        screen.fill((30, 30, 30))
+    if not jogo_finalizado:
+        grid.update(dt)
 
+    screen.fill((25, 25, 30))
 
-        for obj in objects:
-            obj.draw()
+    if jogo_finalizado:
+        t1 = fonte_titulo.render("Jogo Finalizado!", True, (240, 200, 50))
+        t2 = fonte_grande.render("Você completou todas as fases.", True, (200, 200, 200))
 
-        pygame.display.flip()
+        screen.blit(t1, (LARGURA // 2 - t1.get_width() // 2, ALTURA // 2 - 100))
+        screen.blit(t2, (LARGURA // 2 - t2.get_width() // 2, ALTURA // 2 - 30))
+
+        pygame.draw.rect(screen, (50, 130, 70), btn_recomecar, border_radius=6)
+        pygame.draw.rect(screen, (90, 200, 110), btn_recomecar, 2, border_radius=6)
+
+        txt_b = fonte.render("Jogar De Novo", True, (255, 255, 255))
+        screen.blit(txt_b,
+                    (btn_recomecar.centerx - txt_b.get_width() // 2, btn_recomecar.centery - txt_b.get_height() // 2))
+
+    else:
+        grid.draw(screen)
+
+        txt_n = fonte.render(f"Fase: {nivel_atual + 1}/{len(MAPAS)}", True, (200, 200, 200))
+        txt_m = fonte.render(f"Movimentos: {grid.movimentos}", True, (200, 200, 200))
+        screen.blit(txt_n, (25, 20))
+        screen.blit(txt_m, (25, 45))
+
+        pygame.draw.rect(screen, (60, 65, 75), btn_reset, border_radius=5)
+        pygame.draw.rect(screen, (100, 105, 120), btn_reset, 1, border_radius=5)
+        txt_r = fonte.render("Reiniciar (R)", True, (255, 255, 255))
+        screen.blit(txt_r, (btn_reset.x + 12, btn_reset.y + 6))
+
+        if grid.venceu:
+            bg_vitoria = pygame.Rect(LARGURA // 2 - 160, ALTURA - 85, 320, 50)
+            pygame.draw.rect(screen, (40, 120, 60), bg_vitoria, border_radius=6)
+            pygame.draw.rect(screen, (80, 200, 110), bg_vitoria, 2, border_radius=6)
+
+            txt_v = fonte_grande.render("Fase Concluída!", True, (255, 255, 255))
+            screen.blit(txt_v, (bg_vitoria.centerx - txt_v.get_width() // 2, bg_vitoria.y + 8))
+
+            pygame.draw.rect(screen, (40, 110, 70), btn_prox, border_radius=5)
+            pygame.draw.rect(screen, (80, 180, 110), btn_prox, 1, border_radius=5)
+
+            lbl = "Finalizar" if nivel_atual + 1 == len(MAPAS) else "Próximo >"
+            txt_p = fonte.render(lbl, True, (255, 255, 255))
+            screen.blit(txt_p, (btn_prox.centerx - txt_p.get_width() // 2, btn_prox.y + 6))
+
+    pygame.display.flip()
